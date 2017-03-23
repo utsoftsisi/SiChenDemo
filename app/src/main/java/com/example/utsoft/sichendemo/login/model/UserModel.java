9e57cf8d676bfba31846eaba3a7c254cbfa841be
陈思 <si.chen@utsoft.cn>
2017-03-23 10:11:48 +0800

    增加注释：对MVP模式的理解。

--- app/src/main/java/com/example/utsoft/sichendemo/login/model/UserModel.java
+++ app/src/main/java/com/example/utsoft/sichendemo/login/model/UserModel.java
@@ -4,9 +4,17 @@ import com.example.utsoft.sichendemo.interfaces.OnListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
 /**
- * Created by chensi on 2017/3/20.
+ * Created by 陈思 on 2017/3/23 9:56.
+ * Function:
+ * Desc:
+ * View与Model并不直接交互，而是使用Presenter作为View与Model之间的桥梁。
+ * 其中Presenter中同时持有Viwe层以及Model层的Interface的引用，而View层持有Presenter层Interface的引用。
+ * 当View层某个界面需要展示某些数据的时候，首先会调用Presenter层的某个接口，
+ * 然后Presenter层会调用Model层请求数据，当Model层数据加载成功之后会调用Presenter层的回调方法通知Presenter层数据加载完毕，
+ * 最后Presenter层再调用View层的接口将加载后的数据展示给用户，这就是MVP模式的整个核心过程。
+ * 这样分层的好处就是大大减少了Model与View层之间的耦合度，
+ * 一方面可以使得View层和Model层单独开发与测试，互不依赖，另一方面Model层可以封装复用，可以极大的减少代码量。
  */
 
 public class UserModel implements IUser {
